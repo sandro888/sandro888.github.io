505946d3ae32868ccae8dd313a928c94f9368f18
alex <akhvle88@gmail.com>
2018-07-30 00:58:15 +0400

    c

---
+++ colour picker/js/app.js
@@ -0,0 +1,24 @@
+let result1=document.querySelector(".first3");
+let result2=document.querySelector(".second3");
+let result3=document.querySelector(".third3");
+function colorPicker(pick){
+    if (pick.target.parentNode.className== "one"){
+        result1.style.backgroundColor=document.defaultView.getComputedStyle(pick.target).getPropertyValue("background-color");
+    }else if (pick.target.parentNode.className== "two"){
+        result2.style.backgroundColor=document.defaultView.getComputedStyle(pick.target).getPropertyValue("background-color");
+    }else{
+        result3.style.backgroundColor=document.defaultView.getComputedStyle(pick.target).getPropertyValue("background-color");
+    }
+}
+function saveColors(){
+    let colors =document.querySelectorAll("#add");
+    const chosen=Array.from(colors);
+    chosen.forEach(function(color){
+        color.addEventListener("click", colorPicker)
+
+    })
+
+}
+saveColors();
+
+
