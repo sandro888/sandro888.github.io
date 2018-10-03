7b3a99a32eea3a5869b502304eb5118c7b917a99
sandro888 <36017398+sandro888@users.noreply.github.com>
2018-10-03 15:28:01 +0400

    comit

--- project-2/js/app.js
+++ project-2/js/app.js
@@ -218,9 +218,10 @@ class Food {
     }
     console.log(this.coordinatesArray);
     // Function for generating a random whole number aligned on the game grid
-    function getRandomGameCoordinate(cor) {
-      cor = Math.floor((Math.random() * S + 1));
-      return S * Math.floor(Math.random() * (cor));
+    function getRandomGameCoordinate(range) {
+      let squaresInRange = Math.floor(range/S);
+      let randomCoor = Math.floor(Math.random()*squaresInRange)*S;
+      return randomCoor;
     }
   }
   drawFood() {
