c2ed39e62ebb8e80f9a25d600bdcbfdd74d19ad5
sandro888 <36017398+sandro888@users.noreply.github.com>
2018-10-03 10:54:03 +0400

    comit

--- project-2/js/app.js
+++ project-2/js/app.js
@@ -189,7 +189,16 @@ class Snake {
     });
   };
 }
+const body = document.body;
+const btn = document.querySelectorAll('.button')[0];
 
+btn.addEventListener('mouseenter', () => {
+	body.classList.add('show');
+});
+
+btn.addEventListener('mouseleave', () => {
+	body.classList.remove('show');
+});
 let snake = new Snake()
 
 class Food {
